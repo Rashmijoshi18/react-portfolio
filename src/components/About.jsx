8c8df4c1f7fad35bc28d1f266dbd52b61f43ea5e
Rashmi Joshi <rashmijoshi3699@gmail.com>
2025-06-30 12:01:05 +0530

    about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,19 +14,19 @@ const About = () => {
                     <img src={me} alt="Your profile" className="profile-image" />
                 </div>
                 <div className="about-text">
-                    <h3 className="section-title">Who am I?</h3>
+                    <h2 className="section-title">👋Who am I?</h2>
                     <p>
                         I'm a passionate <strong>Web Developer</strong> focused on building clean,
                         user-friendly websites and enjoy exploring new techniques
                         to create impactful digital solutions.
                     </p>
-                    <h3 className="section-title">Education</h3>
+                    <h2 className="section-title">🎓Education</h2>
                     <p>
                         I'm currently pursuing a <strong>B.Tech in Computer Science and Engineering</strong> to
                         strengthen my technical foundation
                         and to apply my knowledge in practical settings.
                     </p>
-                    <h3 className="section-title">What Drives Me</h3>
+                    <h2 className="section-title">🌱What Drives Me</h2>
                     <p>
                         I'm driven by a love for learning, problem-solving, and creating solutions that make a
                         real impact and believe technology has the power to transform lives.
